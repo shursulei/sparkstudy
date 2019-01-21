75f593a422b659a90e53088be2421513cd612193
shursulei <1368455669@qq.com>
2019-01-21 16:48:19 +0800

    # WARNING: head commit changed in the meantime
    spark学习
    修改

--- src/main/scala/com/shursulei/spark/streaming/adviertisement/AdClickedSteamingStatus.java
+++ src/main/scala/com/shursulei/spark/streaming/adviertisement/AdClickedSteamingStatus.java
@@ -1,20 +1,24 @@
 package com.shursulei.spark.streaming.adviertisement;
-
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.kafka.KafkaUtils;
 public class AdClickedSteamingStatus {
 	public static void main(String[] args) {
-		/*第105课：  Spark Streaming电商广告点击综合案例在线点击统计实战
-	* 广告点击的基本数据格式：timestamp,ip,userID,adID,province,city
-时间、ip、用户ID、广告ID，点击广告所在的省、所在的城市
-	* 至少2条线程，一条线程接受数据，一条处理数据
-	*每个executor 一般分配 多少core？5个core最佳的 分配为奇数个core表现最佳 3个 5个 7个
-	*
-	*/	
+	/*第105课：  Spark Streaming电商广告点击综合案例在线点击统计实战
+	 * 广告点击的基本数据格式：timestamp,ip,userID,adID,province,city
+	 * 时间、ip、用户ID、广告ID，点击广告所在的省、所在的城市
+	 * 至少2条线程，一条线程接受数据，一条处理数据
+	 * 每个executor 一般分配 多少core？5个core最佳的 分配为奇数个core表现最佳 3个 5个 7个
+	 *
+	 */	
 	SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("SparkStreamingWordCountOnline");
- 
-	
 	JavaStreamingContext jsc=new JavaStreamingContext(conf, Durations.seconds(5));
-	
-	 
 	Map kafaParameters = new HashMap();
 	kafaParameters.put("metadata.broker.list", 
 			"master:9092,worker1:9092,worker2:9092");
@@ -44,10 +48,6 @@ public class AdClickedSteamingStatus {
 				 return new Tuple2(clickedRecord, 1L);
 			}
 		});
-	 		
-	    		
-	    		
-	    		
         //new Function2
 	    JavaPairDStream adClickedUsers= pairs.reduceByKey(new Function2() {
 	      @Override
@@ -59,7 +59,6 @@ public class AdClickedSteamingStatus {
 	    /*判断有效的点击，复杂化的采用机器学习训练模型进行在线过滤    简单的根据ip判断1天不超过100次；也可以通过一个batch duration的点击次数
 			判断是否非法广告点击，通过一个batch来判断是不完整的，还需要一天的数据也可以每一个小时来判断。*/
 	    JavaPairDStream    filterClickedBatch = adClickedUsers.filter(new Function, Boolean>() {
-			
 			@Override
 			public Boolean call(Tuple2 v1) throws Exception {
 			if (1 < v1._2){
@@ -67,18 +66,12 @@ public class AdClickedSteamingStatus {
 			} else { 
 				return true;
 					}
-				 
 			}
 		});
-	 
 	    filterClickedBatch.print();
-	
-	    
-	    
 	    jsc.start();
 	    jsc.awaitTermination();
 	    jsc.close();
-	
 	while (true ){	
 	}
 	}
